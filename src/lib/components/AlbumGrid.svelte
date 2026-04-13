08ca39bbfc3b210b0163c9ad188d85a4613361cc
Sluicee <sluicee1@gmail.com>
2026-04-13 20:41:56 +0400

    Я ЧТО-ТО НАЖАЛ И ВСЁ СЛОМАЛОСЬ

--- src/lib/components/AlbumGrid.svelte
+++ src/lib/components/AlbumGrid.svelte
@@ -242,8 +242,18 @@
                 onhover(a);
               }}
             />
+          {/each}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-hidden;
+<style>
+  .wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
     display: flex;
     align-items: center;
     justify-content: center;
