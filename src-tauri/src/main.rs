2a13cdf47a3b3e8c3366205bd4c98995a8a9142e
Sluicee <sluicee1@gmail.com>
2026-07-22 19:37:39 +0400

    chore:  disable explicit sync for NVIDIA drivers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     {
         std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
         std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+        std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
     }
 
     musicplayer_lib::run()
